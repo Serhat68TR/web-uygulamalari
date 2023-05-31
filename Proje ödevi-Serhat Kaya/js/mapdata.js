4cedbb8b3e100ce4cbcb2653f9dc2c0d8307d22e
Serhat68TR <62091884+Serhat68TR@users.noreply.github.com>
2023-05-31 12:20:40 +0300

    Add files via upload

---
+++ Proje ödevi-Serhat Kaya/js/mapdata.js
@@ -0,0 +1,456 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",  
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#c45555",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url:"ic_anadolu_bolgesi.html",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TUR2229: {
+      name: "Aydin"
+    },
+    TUR2230: {
+      name: "Izmir"
+    },
+    TUR2238: {
+      name: "Balikesir"
+    },
+    TUR2239: {
+      name: "Çanakkale"
+    },
+    TUR2240: {
+      name: "Edirne"
+    },
+    TUR2241: {
+      name: "Kirklareli"
+    },
+    TUR2242: {
+      name: "Tekirdag"
+    },
+    TUR2263: {
+      name: "Bilecik"
+    },
+    TUR2264: {
+      name: "Bursa"
+    },
+    TUR2265: {
+      name: "Istanbul"
+    },
+    TUR2266: {
+      name: "Kocaeli"
+    },
+    TUR2267: {
+      name: "Sakarya"
+    },
+    TUR2268: {
+      name: "Bolu"
+    },
+    TUR2269: {
+      name: "Eskisehir"
+    },
+    TUR2270: {
+      name: "Kastamonu"
+    },
+    TUR2271: {
+      name: "Antalya"
+    },
+    TUR2272: {
+      name: "Afyonkarahisar"
+    },
+    TUR2273: {
+      name: "Burdur"
+    },
+    TUR2274: {
+      name: "Denizli"
+    },
+    TUR2275: {
+      name: "Isparta"
+    },
+    TUR2276: {
+      name: "Kütahya"
+    },
+    TUR2277: {
+      name: "Manisa"
+    },
+    TUR2278: {
+      name: "Mugla"
+    },
+    TUR2281: {
+      name: "Adiyaman"
+    },
+    TUR2282: {
+      name: "Elazig"
+    },
+    TUR2283: {
+      name: "K. Maras"
+    },
+    TUR2284: {
+      name: "Malatya"
+    },
+    TUR2285: {
+      name: "Mersin"
+    },
+    TUR2286: {
+      name: "Kirsehir"
+    },
+    TUR2287: {
+      name: "Kayseri"
+    },
+    TUR2288: {
+      name: "Nevsehir"
+    },
+    TUR2289: {
+      name: "Hatay"
+    },
+    TUR2290: {
+      name: "Amasya"
+    },
+    TUR2291: {
+      name: "Çorum"
+    },
+    TUR2292: {
+      name: "Giresun"
+    },
+    TUR2293: {
+      name: "Ordu"
+    },
+    TUR2294: {
+      name: "Sinop"
+    },
+    TUR2295: {
+      name: "Sivas"
+    },
+    TUR2296: {
+      name: "Samsun"
+    },
+    TUR2297: {
+      name: "Tokat"
+    },
+    TUR2298: {
+      name: "Artvin"
+    },
+    TUR2299: {
+      name: "Erzurum"
+    },
+    TUR2300: {
+      name: "Erzincan"
+    },
+    TUR2301: {
+      name: "Rize"
+    },
+    TUR2302: {
+      name: "Trabzon"
+    },
+    TUR2307: {
+      name: "Agri"
+    },
+    TUR2308: {
+      name: "Bingöl"
+    },
+    TUR2309: {
+      name: "Diyarbakir"
+    },
+    TUR2310: {
+      name: "Mus"
+    },
+    TUR2311: {
+      name: "Bitlis"
+    },
+    TUR3008: {
+      name: "Ankara"
+    },
+    TUR3009: {
+      name: "Çankiri"
+    },
+    TUR3010: {
+      name: "Zinguldak"
+    },
+    TUR3011: {
+      name: "Konya"
+    },
+    TUR3012: {
+      name: "Karaman"
+    },
+    TUR3014: {
+      name: "Usak"
+    },
+    TUR3016: {
+      name: "Gaziantep"
+    },
+    TUR3017: {
+      name: "Sanliurfa"
+    },
+    TUR3018: {
+      name: "Adana"
+    },
+    TUR3019: {
+      name: "Kinkkale"
+    },
+    TUR3020: {
+      name: "Nigde"
+    },
+    TUR3021: {
+      name: "Aksaray"
+    },
+    TUR3026: {
+      name: "Yozgat"
+    },
+    TUR3031: {
+      name: "Gümüshane"
+    },
+    TUR3032: {
+      name: "Bayburt"
+    },
+    TUR3040: {
+      name: "Kars"
+    },
+    TUR3041: {
+      name: "Mardin"
+    },
+    TUR3042: {
+      name: "Batman"
+    },
+    TUR3043: {
+      name: "Siirt"
+    },
+    TUR3044: {
+      name: "Sirnak"
+    },
+    TUR3045: {
+      name: "Tunceli"
+    },
+    TUR3047: {
+      name: "Hakkari"
+    },
+    TUR3048: {
+      name: "Van"
+    },
+    TUR4839: {
+      name: "Ardahan"
+    },
+    TUR4840: {
+      name: "Iğdir"
+    },
+    TUR4841: {
+      name: "Kilis"
+    },
+    TUR4842: {
+      name: "Osmaniye"
+    },
+    TUR5518: {
+      name: "Yalova"
+    },
+    TUR5519: {
+      name: "Düzce"
+    },
+    TUR5520: {
+      name: "Karabük"
+    },
+    TUR5521: {
+      name: "Bartın"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "39.911652",
+      lng: "32.840305",
+      name: "Ankara"
+      
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {
+    "0": {
+      states: [
+        "TUR5518",
+        "TUR2242",
+        "TUR2267",
+        "TUR2266",
+        "TUR2241",
+        "TUR2265",
+        "TUR2240",
+        "TUR2239",
+        "TUR2264",
+        "TUR2263",
+        "TUR2238"
+      ],
+      name: "Marmara",
+      url: "marmara_bolgesi.html"
+    },
+    "1": {
+      states: [
+        "TUR2278",
+        "TUR2230",
+        "TUR2229",
+        "TUR2272",
+        "TUR2274",
+        "TUR2277",
+        "TUR2276",
+        "TUR3014"
+      ],
+      name: "Ege",
+      url: "ege_bolgesi.html"
+    },
+    "2": {
+      states: [
+        "TUR2271",
+        "TUR3018",
+        "TUR2285",
+        "TUR2289",
+        "TUR2273",
+        "TUR4842",
+        "TUR2283",
+        "TUR2275"
+      ],
+      name: "Akdeniz",
+      url: "akdeniz_bolgesi.html"
+    },
+    "3": {
+      states: [
+        "TUR3008",
+        "TUR3011",
+        "TUR2287",
+        "TUR2269",
+        "TUR2295",
+        "TUR3019",
+        "TUR3021",
+        "TUR3020",
+        "TUR3012",
+        "TUR2286",
+        "TUR2288",
+        "TUR3026",
+        "TUR3009"
+      ],
+      name: "İç Anadolu",
+      url: "ic_anadolu_bolgesi.html"
+    },
+    "4": {
+      states: [
+        "TUR2307",
+        "TUR2311",
+        "TUR4839",
+        "TUR2308",
+        "TUR2282",
+        "TUR2300",
+        "TUR2299",
+        "TUR3047",
+        "TUR4840",
+        "TUR3040",
+        "TUR2284",
+        "TUR2310",
+        "TUR3045",
+        "TUR3048"
+      ],
+      name: "Doğu Anadolu",
+      url: "dogu_anadolu_bolgesi.html"
+    },
+    "5": {
+      states: [
+        "TUR2290",
+        "TUR2298",
+        "TUR5521",
+        "TUR3032",
+        "TUR2268",
+        "TUR2291",
+        "TUR5519",
+        "TUR3031",
+        "TUR2292",
+        "TUR5520",
+        "TUR2270",
+        "TUR2293",
+        "TUR2294",
+        "TUR2296",
+        "TUR2301",
+        "TUR2297",
+        "TUR2302",
+        "TUR3010"
+      ],
+      name: "karadeniz",
+      url: "karadeniz_bolgesi.html"
+    },
+    "6": {
+      states: [
+        "TUR2281",
+        "TUR3044",
+        "TUR4841",
+        "TUR3042",
+        "TUR3043",
+        "TUR3041",
+        "TUR3017",
+        "TUR2309",
+        "TUR3016"
+      ],
+      name: "guneydogu",
+      url: "guneydogu_anadolu_bolgesi.html"
+    }
+  }
+};
